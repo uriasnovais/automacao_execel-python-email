762844e1b17a3a26aa663da2c9a27bb084707cef
UriasNovais <j.uriasnovais@gmail.com>
2022-03-08 10:57:20 -0300

    att visualização

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
+#importar o pandas
+import pandas as pd
+
 # importar base de dados
+tabela_vendas = pd.read_excel('Vendas.xlsx')
+
 # visualizar base de dados
+pd.set_option('display.max.column', None)
+print(tabela_vendas)
+
 # faturamento por loja
 # total vendido por loja
 # ticket médio por produto
 # enviar e-mail
 
-import pandas as pd
 
